0745f4934b4d91dbdefdd8aac90f6f9bc6993c3b
lichunyu <lichunyu@yzsbank.com>
2018-12-26 16:19:20 +0800

    SQL语句

---
+++ spring-boot-web/src/main/resources/static/db2/DB2ADMIN_INIT_CREATE.sql
@@ -0,0 +1,521 @@
+create table CUSTOMER_APPROVAL
+(
+  ID                 BIGINT generated always as identity
+    constraint CUSTOMER_APPROVAL_PK
+      primary key,
+  INTERVIEW_ID       BIGINT,
+  ID_NUMBER          VARCHAR(20),
+  CUSTOMER_ID        BIGINT,
+  APPROVAL_OPINION   VARCHAR(200),
+  APPROVAL_ROLE_ID   BIGINT,
+  APPROVAL_USER_NAME VARCHAR(40),
+  APPROVAL_USER_ID   BIGINT,
+  APPROVAL_RESULT    VARCHAR(5),
+  APPROVAL_NODE      VARCHAR(5),
+  CREATED_AT         BIGINT not null,
+  UPDATED_AT         BIGINT not null
+);
+
+create table CUSTOMER_BLACK
+(
+  ID            BIGINT generated always as identity
+    constraint CUSTOMER_BLACK_PK
+      primary key,
+  CUSTOMER_ID   BIGINT,
+  CUSTOMER_NAME VARCHAR(40),
+  HOUSEHOLD_ID  VARCHAR(40),
+  ID_NUMBER     VARCHAR(20),
+  REASON        VARCHAR(600),
+  PHONE         VARCHAR(20),
+  GRID_CODE     VARCHAR(40),
+  GRID_NAME     VARCHAR(100),
+  ORG_CODE      VARCHAR(60),
+  ORG_NAME      VARCHAR(100),
+  USER_ID       BIGINT,
+  USER_NAME     VARCHAR(40),
+  STATUS        VARCHAR(5),
+  UPDATED_AT    BIGINT not null,
+  CREATED_AT    BIGINT not null
+);
+
+create table CUSTOMER_GREY
+(
+  ID            BIGINT generated always as identity
+    constraint CUSTOMER_GREY_PK
+      primary key,
+  CUSTOMER_ID   BIGINT,
+  CUSTOMER_NAME VARCHAR(60),
+  HOUSEHOLD_ID  VARCHAR(20),
+  ID_NUMBER     VARCHAR(18) not null,
+  REASON        VARCHAR(100),
+  PHONE         VARCHAR(20),
+  GRID_CODE     VARCHAR(20),
+  GRID_NAME     VARCHAR(50),
+  ORG_CODE      VARCHAR(20),
+  ORG_NAME      VARCHAR(50),
+  USER_ID       BIGINT,
+  USER_NAME     VARCHAR(60),
+  STATUS        VARCHAR(5),
+  CREATED_AT    BIGINT      not null,
+  UPDATED_AT    BIGINT      not null
+);
+
+create table CUSTOMER_IMAGE
+(
+  ID             BIGINT generated always as identity
+    constraint CUSTOMER_IMAGE_PK
+      primary key,
+  CUSTOMER_NAME  VARCHAR(50),
+  ID_NUMBER      VARCHAR(20) not null,
+  GRID_CODE      VARCHAR(20) not null,
+  ORIGINAL_NAME  VARCHAR(150),
+  SYSTEM_NAME    VARCHAR(150),
+  PATH           VARCHAR(200),
+  UPLOAD_USER_ID BIGINT,
+  TYPE           VARCHAR(10),
+  DELETE_FLAG    VARCHAR(10),
+  DELETE_PATH    VARCHAR(150),
+  DELETE_TIME    BIGINT,
+  CREATED_AT     BIGINT,
+  UPDATED_AT     BIGINT
+);
+
+comment on table CUSTOMER_IMAGE is '客户影像资料表';
+
+create table CUSTOMER_INTERVIEW
+(
+  ID                  BIGINT generated always as identity
+    constraint CUSTOMER_INTERVIEW_PK
+      primary key,
+  CUSTOMER_ID         BIGINT,
+  CUSTOMER_NAME       VARCHAR(40),
+  ID_NUMBER           VARCHAR(20),
+  SEX                 VARCHAR(5),
+  IS_MARRIED          VARCHAR(10),
+  EDUCATION           VARCHAR(10),
+  PHONE               VARCHAR(20),
+  FAMILY_SIZE         VARCHAR(5),
+  NATIVE_ADDRESS      VARCHAR(100),
+  RESIDENCE_ADDRESS   VARCHAR(100),
+  CARD_HOLDER         VARCHAR(40),
+  CARD_NUMBER         VARCHAR(40),
+  APPRAISE_SUM        VARCHAR(30),
+  APPLIED_SUM         VARCHAR(30),
+  TIME_LIMIT          VARCHAR(5),
+  PROVIDE             VARCHAR(30),
+  REPAYMENT           VARCHAR(30),
+  INTEREST_SETTLEMENT VARCHAR(30),
+  TYPE                VARCHAR(5),
+  USER_ID             BIGINT,
+  USER_NAME           VARCHAR(40),
+  ORG_CODE            VARCHAR(100),
+  ORG_NAME            VARCHAR(100),
+  GRID_NAME           VARCHAR(100),
+  GRID_CODE           VARCHAR(40),
+  COMMENT             VARCHAR(100),
+  APPROVAL_STATUS     VARCHAR(5),
+  STATUS              VARCHAR(5),
+  ATTACH_FLAG         VARCHAR(5),
+  CREATED_AT          BIGINT not null,
+  UPDATED_AT          BIGINT not null,
+  PURPOSE             VARCHAR(40),
+  HOUSEHOLD_ID        VARCHAR(40)
+);
+
+create table CUSTOMER_POVERTY
+(
+  ID            BIGINT generated always as identity
+    constraint CUSTOMER_POVERTY_PK
+      primary key,
+  CUSTOMER_ID   BIGINT,
+  CUSTOMER_NAME VARCHAR(100),
+  HOUSEHOLD_ID  VARCHAR(60),
+  ID_NUMBER     VARCHAR(20),
+  REASON        VARCHAR(150),
+  PHONE         VARCHAR(20),
+  GRID_CODE     VARCHAR(100),
+  GRID_NAME     VARCHAR(100),
+  ORG_CODE      VARCHAR(100),
+  ORG_NAME      VARCHAR(100),
+  USER_ID       BIGINT,
+  USER_NAME     VARCHAR(100),
+  STATUS        VARCHAR(5),
+  CREATED_AT    BIGINT not null,
+  UPDATED_AT    BIGINT not null
+);
+
+create table CUSTOMER_WHITE
+(
+  ID            BIGINT generated always as identity
+    constraint CUSTOMER_WHITE_PK
+      primary key,
+  CUSTOMER_ID   BIGINT,
+  CUSTOMER_NAME VARCHAR(40),
+  ID_NUMBER     VARCHAR(18) not null,
+  LIMIT         DECIMAL(10, 4),
+  PHONE         VARCHAR(20),
+  GRID_CODE     VARCHAR(20) not null,
+  GRID_NAME     VARCHAR(100),
+  ORG_CODE      VARCHAR(20) not null,
+  ORG_NAME      VARCHAR(100),
+  USER_ID       BIGINT      not null,
+  USER_NAME     VARCHAR(40),
+  STATUS        VARCHAR(5) default '0',
+  CREATED_AT    BIGINT      not null,
+  UPDATED_AT    BIGINT      not null,
+  HOUSEHOLD_ID  VARCHAR(20)
+);
+
+create table DB_CUSTOMER_TAG_RELATION
+(
+  ID        BIGINT generated always as identity
+    constraint PK_ID
+      primary key,
+  TAG_ID    BIGINT      not null,
+  ID_NUMBER VARCHAR(50) not null
+);
+
+create table DB_ORG
+(
+  ID              BIGINT generated always as identity
+    constraint PK_ID
+      primary key,
+  ORG_CODE        VARCHAR(20)  not null,
+  CORE_ORG_CODE   VARCHAR(20)  not null,
+  ORG_NAME        VARCHAR(200) not null,
+  PARENT_ORG_CODE VARCHAR(20)  not null,
+  CORP_ORG_CODE   VARCHAR(20),
+  LEVEL           VARCHAR(2)   not null,
+  REMARK          VARCHAR(400),
+  STATUS          VARCHAR(2)   not null,
+  CREATED_AT      BIGINT       not null,
+  UPDATED_AT      BIGINT       not null
+);
+
+create unique index UK_CORE_ORG_CODE
+  on DB_ORG (CORE_ORG_CODE);
+
+create unique index UK_ORG_CODE
+  on DB_ORG (ORG_CODE);
+
+create unique index UK_ORG_NAME
+  on DB_ORG (ORG_NAME);
+
+create table DB_PERMISSION
+(
+  ID             BIGINT generated always as identity
+    primary key,
+  PERMISSION_CH  VARCHAR(80)  not null,
+  PERMISSION_URL VARCHAR(100) not null,
+  CREATED_AT     BIGINT       not null,
+  UPDATED_AT     BIGINT       not null
+);
+
+create table DB_ROLE
+(
+  ID         BIGINT generated always as identity
+    primary key,
+  ROLE_EN    VARCHAR(20) not null,
+  ROLE_CH    VARCHAR(40) not null,
+  CREATED_AT BIGINT      not null,
+  UPDATED_AT BIGINT      not null
+);
+
+comment on column DB_ROLE.ROLE_EN is '角色英文名';
+
+comment on column DB_ROLE.ROLE_CH is '角色中文名';
+
+create table DB_ROLE_PERMISSION_RELATION
+(
+  ID            BIGINT generated always as identity
+    primary key,
+  ROLE_ID       BIGINT not null,
+  PERMISSION_ID BIGINT not null,
+  CREATED_AT    BIGINT not null,
+  UPDATED_AT    BIGINT not null
+);
+
+create table DB_TAG
+(
+  ID          BIGINT generated always as identity
+    constraint PK_ID
+      primary key,
+  NAME        VARCHAR(50)  not null,
+  DESCRIPTION VARCHAR(100) not null,
+  TYPE        VARCHAR(40)  not null,
+  REMARK      VARCHAR(200),
+  CREATED_AT  BIGINT       not null,
+  UPDATED_AT  BIGINT       not null
+);
+
+create unique index UK_NAME
+  on DB_TAG (NAME);
+
+create table DB_USER
+(
+  ID            BIGINT generated always as identity
+    constraint PK_ID
+      primary key,
+  USERNAME      VARCHAR(20)  not null,
+  PASSWORD      VARCHAR(100) not null,
+  NAME          VARCHAR(50)  not null,
+  WORK_ID       VARCHAR(20)  not null,
+  ID_NUMBER     VARCHAR(18),
+  GENDER        VARCHAR(2),
+  CONTACT       VARCHAR(30),
+  EMAIL         VARCHAR(50),
+  ORG_CODE      VARCHAR(50)  not null,
+  CORP_NAME     VARCHAR(50)  not null,
+  STATUS        VARCHAR(2)   not null,
+  LAST_LOGIN_AT BIGINT,
+  CREATED_AT    BIGINT       not null,
+  UPDATED_AT    BIGINT       not null
+);
+
+create unique index UK_USERNAME
+  on DB_USER (USERNAME);
+
+create table DB_USER_ROLE_RELATION
+(
+  ID         BIGINT generated always as identity
+    primary key,
+  USER_ID    BIGINT not null,
+  ROLE_ID    BIGINT not null,
+  CREATED_AT BIGINT not null,
+  UPDATED_AT BIGINT not null
+);
+
+
+create table GRID_IMAGE
+(
+  ID            BIGINT generated always as identity
+    primary key,
+  GRID_CODE     VARCHAR(20),
+  ORIGINAL_NAME VARCHAR(60),
+  SYSTEM_NAME   VARCHAR(150),
+  PATH          VARCHAR(255),
+  USER_ID       BIGINT,
+  TYPE          VARCHAR(10),
+  STATUS        VARCHAR(10) default '0' not null,
+  DELETE_PATH   VARCHAR(255),
+  DELETE_TIME   BIGINT,
+  CREATED_AT    BIGINT                  not null,
+  UPDATED_AT    BIGINT                  not null,
+  COMMENT       VARCHAR(255),
+  IMAGE_NUM     VARCHAR(40)
+);
+
+create table GRID_INFO
+(
+  ID                BIGINT generated always as identity
+    primary key,
+  GRID_CODE         VARCHAR(100),
+  GRID_NAME         VARCHAR(100),
+  ORG_CODE          VARCHAR(20),
+  GRID_TYPE         VARCHAR(5),
+  DESCRIPTION       VARCHAR(255),
+  USER_ID           BIGINT                  not null,
+  ASSIST_MANAGER    VARCHAR(50),
+  SUPERVISE_MANAGER VARCHAR(50),
+  QR_CODE           VARCHAR(255),
+  GRID_MAP          VARCHAR(255),
+  CREATED_AT        BIGINT,
+  UPDATED_AT        BIGINT,
+  STATUS            VARCHAR(10) default '1' not null,
+  TOWNSHIP          VARCHAR(100),
+  VILLAGE           VARCHAR(100),
+  GROUP             VARCHAR(100)
+);
+
+create table GRID_REVIEW
+(
+  ID               BIGINT generated always as identity
+    primary key,
+  GRID_CODE        VARCHAR(100),
+  GRID_NAME        VARCHAR(200),
+  PHONE            VARCHAR(20),
+  GRID_REVIEW_NAME VARCHAR(50),
+  ID_NUMBER        VARCHAR(20),
+  DUTIES           VARCHAR(20),
+  DESCRIPTION      VARCHAR(255),
+  ADDRESS          VARCHAR(255),
+  CREATED_AT       BIGINT not null,
+  UPDATED_AT       BIGINT not null,
+  TYPE             VARCHAR(10),
+  STATUS           VARCHAR(5) default '1'
+);
+
+create table INTERVIEW_FILE
+(
+  ID            BIGINT generated always as identity
+    constraint INTERVIEW_FILE_PK
+      primary key,
+  CUSTOMER_NAME VARCHAR(40),
+  ID_NUMBER     VARCHAR(20),
+  CUSTOMER_ID   BIGINT,
+  INTERVIEW_ID  BIGINT,
+  SYSTEM_NAME   VARCHAR(100),
+  PATH          VARCHAR(200),
+  USER_NAME     VARCHAR(40),
+  USER_ID       BIGINT,
+  TYPE          VARCHAR(5),
+  STATUS        VARCHAR(5),
+  DELETE_PATH   VARCHAR(200),
+  DELETE_TIME   BIGINT,
+  CREATED_AT    BIGINT not null,
+  UPDATED_AT    BIGINT not null
+);
+
+
+create table USER_CONCLUSION
+(
+  ID              BIGINT generated always as identity
+    primary key,
+  NAME            VARCHAR(50),
+  ID_NUMBER       VARCHAR(18) not null,
+  HOUSEHOLD_ID    VARCHAR(20),
+  IS_FAMILIAR     VARCHAR(4),
+  NEGATIVE_REASON VARCHAR(80),
+  REMARK          VARCHAR(900),
+  OUT_WORK        VARCHAR(300),
+  HOUSE_VALUE     VARCHAR(20),
+  CAR_VALUE       VARCHAR(20),
+  MAIN_BUSINESS   VARCHAR(100),
+  SCALE           VARCHAR(100),
+  INCOME          VARCHAR(20),
+  PAYOUT          VARCHAR(20),
+  CREDIT_AMOUNT   VARCHAR(20),
+  BORROWER        VARCHAR(20),
+  SURVEY_TYPE     VARCHAR(20),
+  USER_ID         BIGINT,
+  IS_VALID        VARCHAR(4),
+  DATE            VARCHAR(20),
+  CREATED_AT      BIGINT      not null,
+  UPDATED_AT      BIGINT      not null
+);
+
+create table USER_CUSTOMER
+(
+  ID                   BIGINT generated always as identity
+    constraint PK_ID
+      primary key,
+  CREDIT_ID            VARCHAR(20),
+  NAME                 VARCHAR(40) not null,
+  TYPE                 VARCHAR(20) not null,
+  ID_TYPE              VARCHAR(20) not null,
+  ID_NUMBER            VARCHAR(18) not null,
+  SIGN_ORG             VARCHAR(40),
+  IS_LONG_TERM         VARCHAR(4),
+  SIGN_DATE            VARCHAR(20),
+  DUE_DATE             VARCHAR(20),
+  DISTRICT_ADDRESS     VARCHAR(200),
+  BIRTHDAY             VARCHAR(20),
+  GENDER               VARCHAR(4),
+  DETAIL_ADDRESS       VARCHAR(200),
+  POSTCODE             VARCHAR(6),
+  NATIVE_ADDRESS       VARCHAR(200),
+  PHONE_NUMBER         VARCHAR(20),
+  NATIONALITY          VARCHAR(2),
+  NATION               VARCHAR(2),
+  POLITICS_STATUS      VARCHAR(2),
+  EDUCATION_BACKGROUND VARCHAR(2),
+  PHYSICAL_CONDITION   VARCHAR(2),
+  MARITAL_STATUS       VARCHAR(2),
+  SPOUSE_NAME          VARCHAR(40),
+  SPOUSE_ID_NUMBER     VARCHAR(18),
+  SPOUSE_PHONE_NUMBER  VARCHAR(11),
+  SPOUSE_COMPANY_NAME  VARCHAR(100),
+  CAREER_TYPE          VARCHAR(2),
+  TAX_TYPE             VARCHAR(40),
+  MAIN_BUSINESS        VARCHAR(40),
+  INDUSTRY_TYPE        VARCHAR(60),
+  IS_STAFF             VARCHAR(4),
+  IS_STOCKHOLDER       VARCHAR(4),
+  IS_CIVIL_SERVANT     VARCHAR(4),
+  IS_HOUSE_OWNER       VARCHAR(4),
+  HOUSEHOLD_ID         VARCHAR(20),
+  COMPANY_NAME         VARCHAR(100),
+  WORK_YEAR            VARCHAR(2),
+  COMPANY_ADDRESS      VARCHAR(200),
+  IS_FARMER            VARCHAR(4),
+  IS_BUY_HOME          VARCHAR(4),
+  CELL_NAME            VARCHAR(40),
+  CELL_ADDRESS         VARCHAR(200),
+  LIVING_CONDITION     VARCHAR(40),
+  IS_BUSINESS_OWNER    VARCHAR(4),
+  IS_BORROWER          VARCHAR(4),
+  GRID_CODE            VARCHAR(20),
+  CUSTOMER_MANAGER     VARCHAR(40),
+  SUPPORT_MANAGER      VARCHAR(40),
+  MAIN_ORG_NAME        VARCHAR(200),
+  REGISTER_PERSON      VARCHAR(40),
+  LAST_MODIFY_PERSON   VARCHAR(40),
+  STATUS               VARCHAR(2)  not null,
+  CREATED_AT           BIGINT      not null,
+  UPDATED_AT           BIGINT      not null,
+  INTERVIEW_ID         BIGINT,
+  AMOUNT               VARCHAR(18),
+  VALID_TIME           INTEGER        default 0,
+  IS_CONCLUDED         VARCHAR(4),
+  RELATIONSHIP         VARCHAR(50),
+  AGE                  INTEGER,
+  OUT_ENSURE_AMOUNT    DECIMAL(18, 2) default 0
+);
+
+create unique index UK_ID_NUMBER
+  on USER_CUSTOMER (ID_NUMBER);
+
+
+
+create table USER_RESIDENT
+(
+  ID           BIGINT generated always as identity
+    primary key,
+  NAME         VARCHAR(50),
+  ID_NUMBER    VARCHAR(20) not null
+    unique,
+  CONTACT      VARCHAR(40),
+  COUNTY       VARCHAR(100),
+  TOWNSHIP     VARCHAR(100),
+  VILLAGE      VARCHAR(150),
+  GROUP        VARCHAR(150),
+  HOUSEHOLD_ID VARCHAR(20),
+  RELATIONSHIP VARCHAR(60),
+  IS_IN_LIST   VARCHAR(20),
+  REMARK       VARCHAR(500),
+  USER_ID      BIGINT,
+  USER_NAME    VARCHAR(50),
+  ORG_NAME     VARCHAR(200),
+  GRID_CODE    VARCHAR(40),
+  CREATED_AT   BIGINT      not null,
+  UPDATED_AT   BIGINT      not null
+);
+
+create table USER_SURVEY
+(
+  ID              BIGINT generated always as identity
+    primary key,
+  NAME            VARCHAR(20),
+  ID_NUMBER       VARCHAR(18) not null,
+  HOUSEHOLD_ID    VARCHAR(20),
+  IS_FAMILIAR     VARCHAR(4),
+  NEGATIVE_REASON VARCHAR(80),
+  REMARK          VARCHAR(300),
+  OUT_WORK        VARCHAR(100),
+  HOUSE_VALUE     VARCHAR(20),
+  CAR_VALUE       VARCHAR(20),
+  MAIN_BUSINESS   VARCHAR(100),
+  SCALE           VARCHAR(100),
+  INCOME          VARCHAR(20),
+  PAYOUT          VARCHAR(20),
+  CREDIT_AMOUNT   VARCHAR(20),
+  BORROWER        VARCHAR(40),
+  SURVEY_TYPE     VARCHAR(20),
+  SENATOR         VARCHAR(20),
+  IS_VALID        VARCHAR(4),
+  DATE            VARCHAR(20),
+  CREATED_AT      BIGINT      not null,
+  UPDATED_AT      BIGINT      not null
+);
+
+
